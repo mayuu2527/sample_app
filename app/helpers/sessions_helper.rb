b600db95b7880221d1b699003111f46a4887972a
mayuu <mayusola2527@gmail.com>
2015-01-04 02:35:14 +0900

    Finish user edit, update, index, and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -9,7 +9,7 @@ module SessionsHelper
 
     def signed_in?
     !current_user.nil?
-  end
+    end
 
   def current_user=(user)
     @current_user = user
@@ -20,8 +20,21 @@ module SessionsHelper
     @current_user ||= User.find_by(remember_token: remember_token)
   end
 
+    def current_user?(user)
+    user == current_user
+     end
+
     def sign_out
     self.current_user = nil
     cookies.delete(:remember_token)
+    end
+   
+    def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    session.delete(:return_to)
+    end
+
+  def store_location
+    session[:return_to] = request.url
   end
 end
